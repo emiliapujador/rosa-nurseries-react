0e14e454364720675775bfc66a1fd0557b5adefa
emiliapujador <emiliapujador@gmail.com>
2023-08-11 16:59:18 +0100

    comments section wip

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -2,20 +2,35 @@ import "./Comments.css";
 
 function Comments() {
   return (
-    <div className="bg-[#E8ECED] h-[500px] lg:h-[500px] md:[400px]">
+    <div className="bg-[#E8ECED] h-[500px] lg:h-[500px] md:[400px] ">
       <h3 className="lg:text-[40px] md:text-[30px] text-[25px] text-center font-serif container mx-auto text-darkGreen px-[30px] md:px-[30px] lg:px-[100px] lg:mt-[60px] mt-[40px] md:pt-[50px] pt-[30px]">
         What our customers say about us
       </h3>
-      <div clasName="flex flex-col md:flex-row lg:flex-row lg:justify-around	md:justify-around	">
-        <div clasName="flex flex-col">
+      <div>
+        <div clasName="flex flex-col items-center justify-center px-[30px]">
           <p clasName="text-[15px] md:text-[20px] lg:text-[25px] text-blackR">
             {" "}
-            “ Excellent treatment by the employees. Incredible variety of
-            plants. I will be back!”.
+            “Excellent treatment by the employees. Incredible variety of plants.
+            I will be back!”.
           </p>
           <p clasName="text-[13px] md:text-[18px] lg:text-[22px] text-blackR">
             Jack Smith
           </p>
+          <div>
+            <p>
+              "Beautiful nursery in the heart of Chelsea. The employees advised
+              me on what I was looking for. Thank you very much!"
+            </p>
+            <p>Michelle Harvey</p>
+          </div>
+          <div>
+            <p>
+              Warm and cordial treatment. The place feels very peaceful. I also
+              bought online and had my order at home in two days. Highly
+              recommended”
+            </p>
+            <p>Samuel Holland</p>
+          </div>
         </div>
       </div>
     </div>
@@ -44,7 +59,7 @@ export default Comments;
 
         <div class="">
           <span class="comment3">
-            “ Warm and cordial treatment. The place feels very peaceful. I also
+            Warm and cordial treatment. The place feels very peaceful. I also
             bought online and had my order at home in two days. Highly
             recommended”.
           </span>
