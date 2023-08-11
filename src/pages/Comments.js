6de298c4ad05bf9c11b784fc0550a3f045a30c00
emiliapujador <emiliapujador@gmail.com>
2023-08-11 21:34:31 +0100

    fix design mistakes

--- src/pages/Comments.js
+++ src/pages/Comments.js
@@ -1,10 +1,10 @@
 export default function Comments() {
   return (
-    <div className="bg-[#E8ECED] flex flex-col justify-center items-center text-center gap-3  lg:gap-5 md:flex-col py-36 lg:py-52">
-      <h3 className="lg:text-[40px] md:text-[30px] text-[25px] text-center font-serif  text-darkGreen px-[30px] md:px-[30px]  md:w-[400px] lg:w-[500px] lg:mb-8">
+    <div className="bg-[#E8ECED] flex flex-col justify-center items-center text-center md:flex-col py-36 lg:py-44">
+      <h3 className="lg:text-[40px] md:text-[30px] text-[25px] text-center font-serif  text-darkGreen px-[30px] md:px-[30px]  md:w-[400px] lg:w-[500px] lg:pb-[70px]">
         What our customers say about us
       </h3>
-      <div className="px-[30px] flex flex-col gap-3 md:flex-col lg:flex-row lg:flex-1 md:gap-0 max-w-[400px] lg:max-w-none">
+      <div className="px-[30px] flex flex-col gap-3 lg:gap-7 md:flex-col lg:flex-row lg:flex-1 md:gap-0 max-w-[400px] lg:max-w-[1500px]">
         <Quote name="Jack Smith">
           "Excellent treatment by the employees. Incredible varity of plants. i
           will be back."
@@ -31,33 +31,4 @@ function Quote({ name, children }) {
       <span className="pt-[10px] text-[14px] font-bold">{name}</span>
     </div>
   );
-}
-
-{
-  /* <div>
-        <div className="flex ">
-          <span class="comment1">
-            “ Excellent treatment by the employees. Incredible variety of
-            plants. I will be back!”.
-          </span>
-          <p class="customer-name">Jack Smith</p>
-        </div>
-
-        <div class="">
-          <span class="comment2">
-            “Beautiful nursery in the heart of Chelsea. The employees advised me
-            on what I was looking for. Thank you very much! ”.
-          </span>
-          <p customer-name>Michelle Harvey</p>
-        </div>
-
-        <div class="">
-          <span class="comment3">
-            Warm and cordial treatment. The place feels very peaceful. I also
-            bought online and had my order at home in two days. Highly
-            recommended”.
-          </span>
-          <p class="costumer-name">Samuel Holland</p>
-        </div>
-      </div> */
 }
